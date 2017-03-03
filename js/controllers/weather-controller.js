0087114f76dd69825e7167d0a9e9a5b66bb642f3
Satish <satishdcpl@gmail.com>
2017-03-02 18:18:09 -0600

    Add files via upload

--- js/controllers/weather-controller.js
+++ js/controllers/weather-controller.js
@@ -3,19 +3,37 @@ weatherApp.controller('weatherController',
 	$scope.selectedCity = geteKeyFromDataStore('WASelectedCity');
 	$scope.cities = ['Plano, TX', 'Richardson, TX'];
 	$scope.currentData = null;
+	$scope.error = null;
+	$scope.forcastData = null;
         $scope.onChange =  function(){
 	     seKeyinDataStore('WASelectedCity',$scope.selectedCity);
              weatherService.getWeatherDataforCity($scope.selectedCity).then(
 		function(response){
-			if(response.status===200){;
-				$scope.currentData = {}	
-				$scope.currentData.location = response.data.query.results.channel.location;
-				$scope.currentData.wind = response.data.query.results.channel.wind;
-				$scope.currentData.astronomy = response.data.query.results.channel.astronomy;
-				$scope.currentData.atmosphere = response.data.query.results.channel.atmosphere;
-				$scope.forcastData = response.data.query.results.channel.item.forecast;
+			if(response.status===200){
+				if(response.data.query.results){
+					$scope.currentData = {};
+					$scope.currentData.location = response.data.query.results.channel.location;
+					$scope.currentData.wind = response.data.query.results.channel.wind;
+					$scope.currentData.astronomy = response.data.query.results.channel.astronomy;
+					$scope.currentData.atmosphere = response.data.query.results.channel.atmosphere;
+					$scope.forcastData = response.data.query.results.channel.item.forecast;
+				}else{
+			           $scope.currentData = null;
+				   $scope.forcastData = null;		
+				   $scope.error	= 'Enter valid City name';
+				}
+			}else{
+				$scope.currentData = null;
+				$scope.forcastData = null;	
+				$scope.error = 'Error getting weather data from Yahoo';	
 			}
-		});
+		},
+                function(error){
+			$scope.currentData = null;
+				$scope.forcastData = null;	
+				$scope.error = 'Error getting weather data from Yahoo';	
+		}
+              );
 	};
 	
 	function seKeyinDataStore(keyName,keyValue){
